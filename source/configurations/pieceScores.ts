cc126f0979ec89954bfb73c5d20d86451d0b2497
HeangLeng Taing (Steven) <taing.steven@gmail.com>
2024-02-02 13:26:36 +0700

    fix score

--- source/configurations/pieceScores.ts
+++ source/configurations/pieceScores.ts
@@ -2,8 +2,8 @@ export enum pieceScores {
   pawn = 10,
   knight = 30,
   bishop = 30,
-  rook = 50,
-  queen = 90,
-  pawn_pro = 90,
+  queen = 50,
+  pawn_pro = 50,
+  rook = 90,
   king = 900,
 }
